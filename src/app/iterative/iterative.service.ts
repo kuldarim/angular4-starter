8c32fef54c9f83739f6831040956c447ae915d3f
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2017-09-01 21:14:30 +0300

    added flattening function

--- src/app/iterative/iterative.service.ts
+++ src/app/iterative/iterative.service.ts
@@ -9,20 +9,19 @@ export interface INode {
 export class IterativeService {
   private _nodes: Array<INode> = [];
 
+  notReduced = [{name: 'root', children: [
+    {name: 'a', children: []},
+    {name: 'b', children: []},
+    {name: 'c', children: [
+      {name: 'd', children: []},
+      {name: 'e', children: []},
+      {name: 'f', children: []},
+     ]},
+  ]}];
+
   constructor() {
-    this._nodes = [{
-      name: 'a',
-      depth: '1',
-    }, {
-      name: 'b',
-      depth: '1|1'
-    }, {
-      name: 'c',
-      depth: '1'
-    }, {
-      name: 'c',
-      depth: '1|1'
-    }];
+    this._nodes = this.flatten(this.notReduced, undefined);
+    console.log(this._nodes);
   }
 
   get nodes(): Array<INode> {
@@ -35,7 +34,19 @@ export class IterativeService {
     }
     const parent = this.nodes[index];
     if (parent) {
-      this._nodes.splice(index + 1, 0, {name: 'new', depth: `${parent.depth}|1`});
+      this._nodes.splice(index + 1, 0, {name: 'new', depth: `${parent.depth}|`});
     }
+  }
+
+  flatten(arr, depth) {
+    return arr.reduce((explored, toExplore) => {
+      const currentDepth = depth ? depth + '|' : '|';
+      explored.push({name: toExplore.name, depth: currentDepth});
+          return explored.concat(
+            Array.isArray(toExplore.children) && toExplore.children.length
+              ? this.flatten(toExplore.children, currentDepth)
+              : toExplore.children
+          );
+    }, []);
   }
 }
