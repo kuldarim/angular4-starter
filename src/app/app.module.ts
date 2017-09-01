a5d25f93b80e42f3675ce9f3e811554c0bcc16ff
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2017-09-01 18:37:53 +0300

    clean up and linter issues fixing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { TreeNode } from './tree-node/tree-node.component';
-import { NgModule, ApplicationRef } from '@angular/core';
+import { TreeNodeComponent } from './tree-node/tree-node.component';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
@@ -7,11 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { RecursiveComponent } from './recursive/recursive.component';
 import { IterativeComponent } from './iterative/iterative.component';
-import { ApiService } from './shared';
 import { routing } from './app.routing';
-
-
-import { removeNgStyles, createNewHosts } from '@angularclass/hmr';
 
 @NgModule({
   imports: [
@@ -24,28 +20,8 @@ import { removeNgStyles, createNewHosts } from '@angularclass/hmr';
     AppComponent,
     RecursiveComponent,
     IterativeComponent,
-    TreeNode
-  ],
-  providers: [
-    ApiService
+    TreeNodeComponent
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-  constructor(public appRef: ApplicationRef) {}
-  hmrOnInit(store) {
-    console.log('HMR store', store);
-  }
-  hmrOnDestroy(store) {
-    let cmpLocation = this.appRef.components.map(cmp => cmp.location.nativeElement);
-    // recreate elements
-    store.disposeOldHosts = createNewHosts(cmpLocation);
-    // remove styles
-    removeNgStyles();
-  }
-  hmrAfterDestroy(store) {
-    // display new elements
-    store.disposeOldHosts();
-    delete store.disposeOldHosts;
-  }
-}
+export class AppModule {}
